c6a0c069b9bb8f87ed9b29bdc1f20c6e62f9096c
Matheus Vancoll <matheu-s_andre@hotmail.com>
2021-06-30 21:59:37 -0300

    chance layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,110 @@
-import React from 'react'
+import { React, useState} from 'react'
 import './Login.css'
 
 import { Link } from 'react-router-dom'
 
-export default function Login() {
+export default function Acess() {
+
+  const [Acess, setAcess] = useState("Login")
+
+  if(Acess == "Login") {
+    return Login()
+  }
+
+  if(Acess == "Forgot") {
+    return Forgot()
+  }
+
+  if(Acess == "Register") {
+    return Register()
+  } 
+
+function Login() {
   return(
     <div className="login__container">
       <div className="login__title-app">
         <h1>Financoll</h1>
         <h3>Seu Gerenciador financeiro</h3>
       </div>
-
-      <div className="login__input-area">
-        <div className="input-data">
-          <input type="text" placeholder="Insira seu usuário" />
-          <input type="password" placeholder="Insira sua senha" />
-          <button type="submit">
-            <Link to="/dashboard">
-              Acessar
-            </Link>
-          </button>
-          <Link to="/forgotpassword">Redefinir senha</Link>
+      <div className="login__acess">
+        <div className="login__tabs">
+          <div id="tab-active">Login</div>
+          <Link onClick={()=> setAcess("Register")} id="tab-disable">Primeiro Acesso</Link>
+        </div>
+        <div className="login__input">
+          <div className="input-data">
+            <p>Email</p>
+            <input type="text" />
+            <p>Senha</p>
+            <input type="password" />
+          </div>
+          <div className="input-actions">
+            <Link onClick={()=> setAcess("Forgot")}>Esqueceu a senha?</Link>
+            <Link to="/dashboard" id="btnAcess">Acessar</Link>
+          </div>
         </div>
       </div>
-
-      <div className="login__register">
-        <Link to="/register">Cadastre-se</Link>
-      </div>
-
     </div>
   )
+}
+
+function Forgot() {
+  return(
+    <div className="login__container">
+      <div className="login__title-app">
+        <h1>Financoll</h1>
+        <h3>Seu Gerenciador financeiro</h3>
+      </div>
+      <div className="login__acess">
+        <div className="login__tabs">
+          <Link id="tab-disable" onClick={()=> setAcess("Login")}>Login</Link>
+          <div id="tab-active">Redefinir Senha</div>
+        </div>
+        <div className="login__input">
+          <div className="input-data" id="forgot-active">
+            <p>Email</p>
+            <input type="text" />
+          </div>
+          <div className="input-actions" >
+            <Link onClick={()=> setAcess("Login")} id="btnAcess">Redefinir</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Register() {
+  return(
+    <div className="login__container">
+      <div className="login__title-app">
+        <h1>Financoll</h1>
+        <h3>Seu Gerenciador financeiro</h3>
+      </div>
+      <div className="login__acess">
+        <div className="login__tabs">
+          <Link id="tab-disable" onClick={()=> setAcess("Login")}>Login</Link>
+          <Link id="tab-active">Primeiro Acesso</Link>
+        </div>
+        <div className="login__input register">
+          <div className="input-data">
+            <p>Nome Completo</p>
+            <input type="text" />
+            <p>Email</p>
+            <input type="text" />
+            <p>Senha</p>
+            <input type="text" />
+            <p>Confirmação de senha</p>
+            <input type="password" />
+          </div>
+          <div className="input-actions">
+            <Link onClick={()=> setAcess("Forgot")}>Esqueceu a senha?</Link>
+            <Link to="/dashboard" id="btnAcess">Acessar</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 }
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Login from './Login'
-import Register from './Register'
 import Dashboard from './Dashboard'
-import Forgot from './Forgot'
 
 export default function Router() {
   return(
@@ -13,16 +11,8 @@ export default function Router() {
         <Login />
       </Route>
       
-      <Route exact path="/register">
-        <Register />
-      </Route>
-
       <Route exact path="/dashboard">
         <Dashboard />
-      </Route>
-
-      <Route exact path="/forgotpassword">
-        <Forgot />
       </Route>
 
     </Switch>
